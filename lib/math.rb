678c47a7d355fee137e3152ad5fe36a428b6bb98
Kevin Park <kevin.park07@yahoo.com>
2020-07-03 20:07:23 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,13 @@
 def addition(num1, num2)
-
+adding_numbers = num2 + num1
+puts adding_numbers
+adding_numbers
 end
 
 def subtraction(num1, num2)
-
+subtracting_numbers = num2 - num1
+puts subtracting_numbers
+subtracting_numbers
 end
 
 def division(num1, num2)
